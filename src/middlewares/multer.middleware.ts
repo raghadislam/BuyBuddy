8e85d6f9e9994f0c0de1850920fd4ce3a6b018e4
Raghad Islam <raghad.noser.2003@gmail.com>
2025-09-09 20:11:56 +0300

    feat: add multer photos uploader

---
+++ src/middlewares/multer.middleware.ts
@@ -0,0 +1,44 @@
+import multer, { FileFilterCallback } from "multer";
+import { Request } from "express";
+import { HttpStatus } from "../enums/httpStatus.enum";
+import AppError from "../utils/APIError";
+
+const multerStorage = multer.memoryStorage();
+
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+): void => {
+  const { fieldname, mimetype } = file;
+
+  const isImage = mimetype.startsWith("image");
+
+  if (fieldname === "photo") {
+    return isImage
+      ? cb(null, true)
+      : cb(
+          new AppError(
+            "Only image files allowed for photo.",
+            HttpStatus.BadRequest
+          ) as any,
+          false
+        );
+  }
+
+  return cb(
+    new AppError(
+      `Unexpected field '${fieldname}'`,
+      HttpStatus.BadRequest
+    ) as any,
+    false
+  );
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+}).fields([{ name: "photo", maxCount: 1 }]);
+
+export default upload;
