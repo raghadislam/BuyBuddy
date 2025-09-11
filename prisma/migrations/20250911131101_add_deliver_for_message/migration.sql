d0e5c46e6ea2c6b307e0a0b111c7bc179b8c5667
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-09-11 19:43:54 +0300

    fix migration files

--- prisma/migrations/20250911131101_add_deliver_for_message/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."PrivateMessage" ADD COLUMN     "isDelivered" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "public"."PrivateMessageVisibility" ADD COLUMN     "deliveredAt" TIMESTAMP(3);
